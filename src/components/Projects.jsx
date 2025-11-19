a69ec88974847e5ae69e4ac1f1cf9c4c91aa9873
sebrinamusbah <sebrinam@gmail.com>
2025-11-19 18:19:47 +0300

    Initial commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ function Projects() {
       title: "Portfolio Website",
       description: "React + Vite + GitHub Pages deployment",
       tech: ["React", "JavaScript", "CSS"],
-      link: "https://reactmovielist-cx3anp2ky-sebrina-musbah-s-projects.vercel.app",
+      link: "https://react-frontendproject-drr7tgjod-sebrina-musbah-s-projects.vercel.app",
     },
     {
       title: "Food Order System",
